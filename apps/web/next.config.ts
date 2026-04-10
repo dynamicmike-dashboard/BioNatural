e9c19bafcf6955501caae72a491a2d4b3b51537d
chatallday <chatalldaybot@gmail.com>
2026-04-10 15:51:40 -0500

    fix: Implement Official Monorepo Distribution with distDir redirection

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -8,6 +8,7 @@ const withPWA = require("next-pwa")({
 
 const nextConfig: NextConfig = {
   transpilePackages: ["@bionatural/ui"],
+  distDir: "../../.next",
 };
 
 export default withPWA(nextConfig);
